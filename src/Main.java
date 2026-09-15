550e1ad792bc3c8cf5f6a6cdc65f4fbc5fca261d
Oslik9 <vosahlomatej9@seznam.cz>
2026-09-15 08:57:58 +0200

    5 vet o prazdninach

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Matýsek");
+        // Jedl jsem.
+        // Těšil jsem se na programování
+        // O prázdninách mi chyběl pan učitel Knotek
+        // Miluju OAUH.
+        // jsem nejlepší programator
+    }
+}
