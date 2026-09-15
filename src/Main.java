985bcb0245e75ea57eeb908ebade326928052704
Oslik9 <vosahlomatej9@seznam.cz>
2026-09-15 20:24:18 +0200

    nějaké komentáře no

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
         // O prázdninách jsem se fakt těšil na programování a hlavně na pana učitele šimůnka protože je můj vzor.
         // Nemohl jsem se dočkat až zase vejdu dveřma do tak krásné budovy jako je OAUH a uvidím znovu všechny učitele.
+        // Mám hlad
 
     }
 }
